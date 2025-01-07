203c2e4b1b240066f1930dd447fa086deaf5424f
Davi Santos Rodrigues <davi.s.rodrigues2003@gmail.com>
2025-01-07 20:11:56 -0300

    Makefile adaptado para novo módulo

---
+++ include/Admin.hpp
@@ -0,0 +1,7 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+#include "Usuario.hpp"
+class Admin : public Usuario{
+
+};
+#endif
---
+++ include/Usuario.hpp
@@ -0,0 +1,7 @@
+#ifndef USUARIO_H
+#define USUARIO_H
+using namespace std;
+class Usuario{
+
+};
+#endif
