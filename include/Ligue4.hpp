ecc72a16bfed0a6e45f23aecde788e41f6af65f9
Davi Santos Rodrigues <davi.s.rodrigues2003@gmail.com>
2025-01-07 22:34:09 -0300

    Mudanças feitas para o Ligue4 e para o Reversi também conseguirem, depois devo alterar os acessos para private e essas coisinhas

--- include/Ligue4.hpp
+++ include/Ligue4.hpp
@@ -1,13 +1,6 @@
 #ifndef LIGUE4_H
 #define LIGUE4_H
-#include <iostream>
-#include <vector>
-#include <iomanip>
-#include <algorithm>
-#include <string>
-#include <sstream>
-#include <fstream>
-#include <cmath>
+#include "Jogador.hpp"
 #include "JogosTab.hpp"
 using namespace std;
 class Ligue4 : public JogosTab{
--- include/Reversi.hpp
+++ include/Reversi.hpp
@@ -1,13 +1,6 @@
 #ifndef REVERSI_H
 #define REVERSI_H
-#include <iostream>
-#include <vector>
-#include <iomanip>
-#include <algorithm>
-#include <string>
-#include <sstream>
-#include <fstream>
-#include <cmath>
+#include "Jogador.hpp"
 #include "JogosTab.hpp"
 using namespace std;
 class Reversi : public JogosTab{
